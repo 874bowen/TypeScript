c3f392775b8ba7a5a79367016f631b9adb8cd9fb
Ivan Bowen <bowenivan16@gmail.com>
2023-01-03 19:43:52 +0300

    ts syntax and best practices

---
+++ 01Intro/variableMe.ts
@@ -0,0 +1,16 @@
+let greetings: string = "Hello, Ivan";
+// ts also takes the value of your variable and based on it gives it a type (AUTOMATIC DETECTION)
+let myNum = 6;
+console.log(greetings);
+
+// number
+
+let userId: number = 334466.3;
+let userId2: number;
+userId2 = 545;
+
+// boolean
+let isLoggedIn: boolean = false;
+
+// remove the error temporarily
+export {}
