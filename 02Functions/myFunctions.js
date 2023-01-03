7da9e407dd4accd9f8bbecd77fddd86dfeb526ff
Ivan Bowen <bowenivan16@gmail.com>
2023-01-03 20:24:05 +0300

    ts functions part 2: return value type

--- 02Functions/myFunctions.js
+++ 02Functions/myFunctions.js
@@ -2,12 +2,12 @@
 exports.__esModule = true;
 function addTwo(num) {
     return num + 2;
+    // return "hello";
 }
 function getUpper(val) {
     return val.toUpperCase();
 }
-function signUpUser(name, email, isPaid) {
-}
+function signUpUser(name, email, isPaid) { }
 // when you want to pass 2 arguments but you need 3 params use a default value as below:
 var loginUser = function (name, email, isPaid) {
     if (isPaid === void 0) { isPaid = true; }
@@ -16,3 +16,20 @@ addTwo(5);
 getUpper("Ivan");
 signUpUser("ivan", "bowenivan@gmail.com", true);
 loginUser("Ian", "ian@gmail.com");
+// function getValue(myVal: number): boolean {
+//    if (myVal > 5) return true;
+//    return "200 OK"
+// }
+var getHello = function (s) { return ""; };
+var heros = ["thor", "spiderman", "ironman"];
+// const heros = [1, 2, 3];
+heros.map(function (hero) {
+    return "hero is ".concat(hero);
+});
+function consoleError(errMsg) {
+    console.log(errMsg);
+    // return 1;
+}
+function handleError(errMsg) {
+    throw new Error(errMsg);
+}
--- 02Functions/myFunctions.ts
+++ 02Functions/myFunctions.ts
@@ -1,25 +1,44 @@
-function addTwo(num: number) {
+function addTwo(num: number): number {
    
    return num + 2;
+   // return "hello";
 }
 
 function getUpper(val: String){
    return val.toUpperCase();
 }
 
-function signUpUser(name: String, email: string, isPaid: boolean){
-
-}
+function signUpUser(name: String, email: string, isPaid: boolean) { }
 
 // when you want to pass 2 arguments but you need 3 params use a default value as below:
-let loginUser = (name: string, email: string, isPaid: boolean = true) => {
-
-}
+let loginUser = (name: string, email: string, isPaid: boolean = true) => { }
 
 addTwo(5);
 getUpper("Ivan");
 signUpUser("ivan", "bowenivan@gmail.com", true);
 loginUser("Ian", "ian@gmail.com")
 
+// function getValue(myVal: number): boolean {
+//    if (myVal > 5) return true;
+//    return "200 OK"
+// }
+
+const getHello = (s: string):string => { return "" }
+
+const heros = ["thor", "spiderman", "ironman"];
+// const heros = [1, 2, 3];
+
+heros.map((hero): string => {
+   return `hero is ${hero}`;
+});
+
+function consoleError(errMsg: string): void {
+   console.log(errMsg);
+   // return 1;
+}
+
+function handleError(errMsg: string): never {
+   throw new Error(errMsg); 
+}
 // temporarily removes the duplicate func declaration error
 export{}
