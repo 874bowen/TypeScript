6c86c0e77c6ef2b979f414b0a8fb561214b40b01
Ivan Bowen <itbowen@kabarak.ac.ke>
2023-04-04 20:55:55 +0300

    add typescript file

---
+++ 01Intro/src/main.ts
@@ -0,0 +1,16 @@
+/*
+* TypeScript can be statically or dynamically typed language while JavaScript is dynamically typed language
+* Statically-typed languages means that types are checked at compile time
+* Dynamically-typed languages means that types are checked at run time
+
+*/
+
+// TypeScript can infer the type - implicit
+let myName = "Ivan Bowen"
+
+// explicit stating 
+let myAge: number = 20
+
+let isHappy: Boolean;
+
+isHappy = true;
